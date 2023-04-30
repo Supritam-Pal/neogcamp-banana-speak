d1e9b6b56897e6c2730ea9fd74381ffb219f6341
Supritam-Pal <131386290+Supritam-Pal@users.noreply.github.com>
2023-04-30 22:29:17 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+//input
+var username=prompt("give me username")
+// console.log("script is working from a different file")
+
+//processing
+var welcomeMessage="This scripts works"+" "+username;
+
+//output
+alert(welcomeMessage);
