dd9e17f652da508457e56243f69b7c836a7d9876
Supritam-Pal <131386290+Supritam-Pal@users.noreply.github.com>
2023-05-12 15:00:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,9 @@
 var btnTranslate=document.querySelector("#btn-translate");
 console.log(btnTranslate);
 
-// btnTranslate.addEventListener("click",function clickEventHandler(){;
-// console.log("click");
-// })
+btnTranslate.addEventListener("click",function clickEventHandler(){;
+console.log("clicked");
+ })
 
 
 
