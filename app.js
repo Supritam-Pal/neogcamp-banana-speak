315a9a70208db2506e51e7d6b267ee74d4b3437e
Supritam-Pal <131386290+Supritam-Pal@users.noreply.github.com>
2023-05-13 02:00:49 +0530

    Taking input and console

--- app.js
+++ app.js
@@ -8,16 +8,25 @@
 // //output
 // alert(welcomeMessage);
 var btnTranslate=document.querySelector("#btn-translate");
-console.log(btnTranslate);
+var txtInput=document.querySelector("#txt-input");
+console.log(txtInput);
 
-btnTranslate.addEventListener("click",function clickEventHandler(){;
-console.log("clicked");
- })
-
-
-
+// btnTranslate.addEventListener("click",function clickEventHandler(){;
+// console.log("clicked");                                                       callback function
+//  })
 
 
+// btnTranslate.addEventListener("cick",clickHandler())
+
+// function clickHandler(){                                                      callback function
+//     console.log("clicked");
+// };
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input", txtInput.value);
+};
+btnTranslate.addEventListener("click",clickHandler)
 
 
 
