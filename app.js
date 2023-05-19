65c066aea65cbe4fda7d10e47fb2247a035a3c5b
Supritam-Pal <131386290+Supritam-Pal@users.noreply.github.com>
2023-05-20 00:53:44 +0530

    Translate fromEnglish to Banana using VanillaJS

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 // outputDiv.innerText="rup is good ";
-console.log(outputDiv);
+//console.log(outputDiv);
 //console.log(txtInput);
-
+//var serverURL="lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(text){
+    return serverURL +"?"+"text="+ text;
+}
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server!try again after sometime");
+}
 
 // btnTranslate.addEventListener("click",function clickEventHandler(){;
 // console.log("clicked");                                                       callback function
@@ -30,7 +38,20 @@ function clickHandler(){
     // console.log("clicked");
     // console.log("input",outputDiv.innerText);
     // console.log("input", txtInput.value);
-    outputDiv.innerText="ajsjsjsjsjsjsj "+" "+txtInput.value;
+  //  outputDiv.innerText="ajsjsjsjsjsjsj "+" "+txtInput.value;
+  var inputText=txtInput.value  //taking input
+  
+  
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText=json.contents.translated;
+    outputDiv.innerText = translatedText;  // output
+  })
+   // console.log(json.contents.translated))
+     .catch(errorHandler);
+
 };
 btnTranslate.addEventListener("click",clickHandler)
 
